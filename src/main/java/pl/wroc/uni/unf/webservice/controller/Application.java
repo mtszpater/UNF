7123128d21a3241fe7488ecb11d89cacf48c0a65
Mateusz Pater <mtszpater@gmail.com>
2016-08-08 01:22:06 +0200

    added methods to NewsController

--- src/main/java/pl/wroc/uni/unf/webservice/controller/Application.java
+++ src/main/java/pl/wroc/uni/unf/webservice/controller/Application.java
@@ -4,11 +4,16 @@ package pl.wroc.uni.unf.webservice.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
+
+
--- src/main/java/pl/wroc/uni/unf/webservice/controller/NewsController.java
+++ src/main/java/pl/wroc/uni/unf/webservice/controller/NewsController.java
@@ -1,6 +1,9 @@
 package pl.wroc.uni.unf.webservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wroc.uni.unf.domain.entity.News;
@@ -8,11 +11,35 @@ import pl.wroc.uni.unf.domain.entity.News;
 
 
 @RestController
+@RequestMapping("/api/v1")
 public class NewsController {
 
-    @RequestMapping("/news")
-    public News news(@RequestParam(value="name", defaultValue="default") String name) {
-        return new News();
+    @RequestMapping(value = "/news", method = RequestMethod.GET)
+    public News news(
+            @RequestParam(value="id", defaultValue="-1") Long newsId,
+            @RequestParam(value="token", defaultValue="-1") Long userToken) {
+
+        News exampleNews = new News();
+        exampleNews.setDescription("test");
+        exampleNews.setId(newsId);
+
+        return exampleNews;
     }
+
+    @RequestMapping(value = "/news", method = RequestMethod.POST)
+    public ResponseEntity addNews(
+            @RequestParam(value="title", defaultValue="default") String newsTitle,
+            @RequestParam(value="description", defaultValue="default") String newsDescription,
+            @RequestParam(value="token", defaultValue="-1") Long userToken) {
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(value = "/news", method = RequestMethod.DELETE)
+    public ResponseEntity deleteNews(
+            @RequestParam(value="id", defaultValue="default") Long newsId,
+            @RequestParam(value="token", defaultValue="-1") Long userToken) {
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
+
 
 }
